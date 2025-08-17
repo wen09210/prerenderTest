09f691a49d0bf75055664821bf9d324dad926731
wen <wen09210@gmail.com>
2025-08-17 23:42:03 +0800

    feat: setting

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h3>cool</h3>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
