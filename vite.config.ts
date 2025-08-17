af0761e62a7dd6060ab0cdc2d36899ba768473f2
wen <wen09210@gmail.com>
2025-08-17 23:58:35 +0800

    feat: update yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 根據環境變數動態設定 base
+const isVercel = process.env.VERCEL === '1';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/prerenderTest/',
+  base: isVercel ? '/' : '/prerenderTest/',
 })
