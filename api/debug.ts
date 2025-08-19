cba34b07e2a9f4a6902ffcb5eca8980cc3ed15b6
wen <wen09210@gmail.com>
2025-08-19 17:40:09 +0800

    debug api

---
+++ api/debug.ts
@@ -0,0 +1,9 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).json({
+    message: "✅ Rewrite triggered",
+    userAgent: req.headers['user-agent'],
+    originalUrl: req.url
+  })
+}
