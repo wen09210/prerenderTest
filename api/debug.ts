c84d6af9157d55909cd8305afc1ed1842682ef4e
wen <wen09210@gmail.com>
2025-08-19 17:47:45 +0800

    samll test

--- api/debug.ts
+++ api/debug.ts
@@ -1,9 +1,10 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader("X-Debug", "rewrite-hit");
   res.status(200).json({
     message: "✅ Rewrite triggered",
-    userAgent: req.headers['user-agent'],
+    userAgent: req.headers["user-agent"],
     originalUrl: req.url
-  })
+  });
 }
